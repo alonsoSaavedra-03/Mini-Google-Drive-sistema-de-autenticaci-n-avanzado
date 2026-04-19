4b73a22ccef08d7c47a37e308cd57a23f3a85605
Alonso <donayrealonso987@gmail.com>
2026-04-18 23:55:26 -0600

    Creando los primeros archivos

---
+++ views/include/footer.php
@@ -0,0 +1,10 @@
+<div class="">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center mt-5">Mini Google Drive - Sistema de Autenticación Avanzado</h1>
+                <p class="text-center">Bienvenido a nuestro sistema de autenticación avanzado para Mini Google Drive. Aquí podrás gestionar tus archivos de manera segura y eficiente.</p>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ views/users/first-page.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header class="">
+        <?php include('views/include/header.php') ?>
+    </header>
+    <main class="">
+
+    </main>
+    <footer class="">
+        <?php include('views/include/footer.php') ?>
+    </footer>
+
+    <script src="assets/js/first-page.js"></script>
+</body>
+</html>
